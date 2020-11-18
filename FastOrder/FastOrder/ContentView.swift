53212d1c4bf25e0ad8db22991bfc9a14f3ff7c41
Mina Ashna <mina.ashna@jayway.com>
2020-11-18 13:00:25 +0100

    - initial commit

---
+++ FastOrder/FastOrder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastOrder
+//
+//  Created by Mina Ashna on 18/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastOrder/FastOrder/FastOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastOrderApp.swift
+//  FastOrder
+//
+//  Created by Mina Ashna on 18/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FastOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
