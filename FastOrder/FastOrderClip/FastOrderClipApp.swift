016b2f72d238a2d60930f9db68b50f6a266a5c23
Mina Ashna <mina.ashna@jayway.com>
2020-11-18 14:06:38 +0100

    - add AppClip target

---
+++ FastOrder/FastOrderClip/FastOrderClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastOrderClipApp.swift
+//  FastOrderClip
+//
+//  Created by Mina Ashna on 18/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FastOrderClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
