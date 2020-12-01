674e6d972337c171cdb8f78152437cbd94b56465
Claus Simonsen <claus.simonsen@jayway.com>
2020-12-01 10:28:05 +0100

    Replacing an 'if let' with guard to reduce 'pyramid of doom'

--- FastOrder/FastOrder/Model/FastOrderModel.swift
+++ FastOrder/FastOrder/Model/FastOrderModel.swift
@@ -9,25 +9,25 @@ import Foundation
 
 struct FastOrderModel {
     var user: UserProfile
-
+    
     enum PersistanceKeys: String {
         case userProfile = "user-profile"
     }
-
+    
     init() {
         self.user = UserProfile()
     }
-
+    
     func persistUser(user: UserProfile) {
         print("Attempting Persistance of User Profile")
         UserDefaults.standard.set(try? PropertyListEncoder().encode(user), forKey: PersistanceKeys.userProfile.rawValue)
     }
     
     mutating func loadUser() {
-        if let data = UserDefaults.standard.value(forKey: PersistanceKeys.userProfile.rawValue) as? Data {
-            if let user = try? PropertyListDecoder().decode(UserProfile.self, from: data) {
-                self.user = user
-            }
+        guard let data = UserDefaults.standard.value(forKey: PersistanceKeys.userProfile.rawValue) as? Data else { return }
+        
+        if let user = try? PropertyListDecoder().decode(UserProfile.self, from: data) {
+            self.user = user
         }
     }
     
