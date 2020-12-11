3572b7db91d1705bcf08bf9238427adb2b3803c6
Claus Simonsen <claus.simonsen@jayway.com>
2020-12-11 09:43:07 +0100

    Adding queue number reservation request to the 'reserve' function triggered by the 'Reserve' button on the Onboarding screen

--- FastOrder/FastOrder/Onboarding/OnboardingViewModel.swift
+++ FastOrder/FastOrder/Onboarding/OnboardingViewModel.swift
@@ -6,17 +6,39 @@
 //
 
 import Foundation
+import Combine
 
 class OnboardingViewModel: ObservableObject {
     @Published private var fastOrderModel: FastOrderModel = FastOrderModel()
 
     @Published var user = UserProfile()
-
+    @Published var fastOrderReservation: FastOrderQueueReservationResponse = FastOrderQueueReservationResponse(id: "", slug: "")
+    
+    private let fastOrderFetcher = FastOrderFetcher()
+    private var cancellable = Set<AnyCancellable>()
+    
     
     // MARK: - Intent(s)
     
     func reserve() {
         self.fastOrderModel.persistUser(user: user)
+        
+        fastOrderFetcher.queueNumberReservation(forName: user.name, phoneNumber: user.phoneNumber)
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { value in
+                    switch value {
+                    case .failure:
+                        break
+                    case .finished:
+                        break
+                    }
+            },
+                receiveValue: { [weak self] reservation in
+                    guard let self = self else { return }
+                    self.fastOrderReservation = reservation
+            })
+            .store(in: &cancellable)
     }
 
     func loadUser() {
